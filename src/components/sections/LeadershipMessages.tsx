4b8437fd13efcae966a638dcca3bc670770e551c
marwa.shahawy <mohammed.marwa@gmail.com>
2026-03-16 16:36:10 +0200

    fix: shift outlineframe more to the right in leadership section

--- src/components/sections/LeadershipMessages.tsx
+++ src/components/sections/LeadershipMessages.tsx
@@ -153,7 +153,7 @@ export function LeadershipMessages() {
         <img
           src={asset("/images/outlineframe.svg")}
           alt=""
-          className="absolute -left-32 top-1/2 -translate-y-1/2 h-[120%] w-auto opacity-80"
+          className="absolute left-[15%] top-1/2 -translate-y-1/2 h-[120%] w-auto opacity-80"
         />
       </div>
 
