6289fc21352d01dfbeef1b32bd4d36ee52d9d2f4
marwa.shahawy <mohammed.marwa@gmail.com>
2026-03-16 16:33:39 +0200

    fix: increase outlineframe opacity to 90% in leadership section

--- src/components/sections/LeadershipMessages.tsx
+++ src/components/sections/LeadershipMessages.tsx
@@ -153,7 +153,7 @@ export function LeadershipMessages() {
         <img
           src={asset("/images/outlineframe.svg")}
           alt=""
-          className="absolute -right-32 top-1/2 -translate-y-1/2 h-[120%] w-auto opacity-[0.07]"
+          className="absolute -right-32 top-1/2 -translate-y-1/2 h-[120%] w-auto opacity-90"
         />
       </div>
 
