75e9c3c45f0b702efb7dce101100168e1d9af4f1
marwa.shahawy <mohammed.marwa@gmail.com>
2026-03-18 16:02:37 +0200

    fix: enlarge keyfact image to align with grid cards (400px min-h, object-cover)

--- src/components/sections/KeyFacts.tsx
+++ src/components/sections/KeyFacts.tsx
@@ -191,8 +191,8 @@ export function KeyFacts() {
 
           {/* Column 2 */}
           <div className="flex flex-col gap-6 md:gap-8 w-full lg:w-1/3">
-            <div className="kf-grid-item flex-[1.2] relative min-h-[350px] rounded-[16px] overflow-hidden">
-              <img src={asset("/images/keyfact.svg")} className="absolute inset-0 w-full h-full object-contain" alt="" />
+            <div className="kf-grid-item flex-[1.2] relative min-h-[400px] rounded-[16px] overflow-hidden">
+              <img src={asset("/images/keyfact.svg")} className="absolute inset-0 w-full h-full object-cover" alt="" />
             </div>
             <FactCard card={cards[1]} index={1} className="flex-1 min-h-[300px] bg-[#001421]" />
           </div>
